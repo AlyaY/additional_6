35406021abfe22bca86d220fa80bf7ab9de956f0
AlyaY <alya.yurevich@gmail.com>
2017-09-24 22:30:06 +0300

    added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function zeros(expression) {
-  // your solution
+module.exports = function zeros(e) {
+  var two = 0;
+  var five = 0;
+
+  function zero(expression){
+    expression.split('*').forEach(count);
+    return  Math.min(two ,five);
+  }
+
+  function count(n) {
+    var m = (n[n.length-2] == '!') ? 2 : 1;
+    var num = Number.parseInt(n);
+    var start = (num % 2 == 0) ? 2 : 1;
+
+    for (i = start; i <= num; i += m) {
+      five += search(5, i);
+      two += search(2, i);
+    }
+  }
+
+  function search(n, inN) {
+    var count = 0;
+    while(inN % n == 0 ){
+      inN /= n;
+      count++;
+    }
+    return count;
+  }
+
+  return zero(e);
 }
